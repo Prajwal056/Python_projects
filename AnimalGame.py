b838052e9540fd66082d8bae198a3148e00bc009
Kick056 <prajwal.krao.pkr@gmail.com>
2022-06-04 20:03:31 +0530

    Third commit INDENTATION

--- AnimalGame.py
+++ AnimalGame.py
@@ -1,3 +1,6 @@
+from telnetlib import STATUS
+
+
 def check_guess(guess, answer):
     global score
     still_guessing = True
@@ -9,13 +12,13 @@ def check_guess(guess, answer):
             still_guessing = False
         else:
             if attempt < 2:
-                guess = input("Sorry Wrong Answer, try again")
+                guess = input("Sorry Wrong Answer, try again \n")
             attempt = attempt + 1
     if attempt == 3:
         print("The Correct answer is ",answer )
     
 score = 0
-print("Guess the Animal")
+print("Guess the Animal: ")
 guess1 = input("Which bear lives at the North Pole? ")
 check_guess(guess1, "polar bear")
 guess2 = input("Which is the fastest land animal? ")
